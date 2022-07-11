00ccb080fd23c12702f98ee38a17998296411e19
Sharad Gudihal <sharad.gudihal@zee.com>
2022-07-11 22:06:19 +0530

    Commit build

---
+++ build/precache-manifest.3a01544ba1b1ee2c43906b5bce95ea03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a115077591c615b772a",
+    "url": "/translation/static/css/main.afc31497.chunk.css"
+  },
+  {
+    "revision": "5a115077591c615b772a",
+    "url": "/translation/static/js/main.5a115077.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/translation/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/translation/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "770e45767d44ef7549c7",
+    "url": "/translation/static/js/runtime~main.770e4576.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/translation/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5f217e73cb196d77c909d29a4c839af2",
+    "url": "/translation/index.html"
+  }
+];
